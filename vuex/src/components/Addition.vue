bcf695c0b9c289e55d3918453f110ebe8c5cc1ac
方乐 <1963667219@qq.com>
2021-02-17 20:46:38 +0800

    mapState,mapMutation,action进行加法更新count值

--- vuex/src/components/Addition.vue
+++ vuex/src/components/Addition.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="Addition">
-    <h3>当前最新的count值为:</h3>
+    <!-- <h3>当前最新的count值为:{{ $store.state.count }}</h3> -->
+    <h3>当前最新的count值为:{{ count }}</h3>
     <button @click="Addition">+1</button>
+    <button @click="AdditionN">+N</button>
+    <button @click="Additionasync">+1 Async</button>
+    <button @click="AdditionNasync">+N Async</button>
   </div>
 </template>
  
 <script>
+import { mapState, mapMutations } from 'vuex'
 export default {
   name: 'Addition',
   data() {
     return {}
   },
+  computed: {
+    ...mapState(['count']),
+  },
   methods: {
-    Addition() {},
+    Addition() {
+      //只有mutation中的才能触发
+      this.$store.commit('add')
+    },
+    AdditionN() {
+      //commit的作用就是调用某个mutation
+      this.$store.commit('addN', 5)
+    },
+    Additionasync() {
+      this.$store.dispatch('addAsync')
+    },
+    AdditionNasync() {
+      this.$store.dispatch('addNasync', 50)
+    },
   },
 }
 </script>
--- vuex/src/store/index.js
+++ vuex/src/store/index.js
@@ -5,11 +5,34 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0
   },
   mutations: {
+    //mutations中不能写异步的代码
+    add(state) {
+      state.count++
+    },
+    addN(state, step) {
+      state.count += step
+    },
+    sub(state) {
+      state.count--
+    },
+    subN(state, step) {
+      state.count -= step
+    }
   },
   actions: {
+    addAsync(context) {
+      setTimeout(() => {
+        context.commit('add')
+      }, 1000)
+    },
+    addNasync(context, step) {
+      setTimeout(() => {
+        context.commit('addN', step)
+      }, 1000)
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
