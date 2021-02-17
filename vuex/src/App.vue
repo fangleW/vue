7b3825ff16e8e1c68d7b2130d8f59941a5a90ee0
方乐 <1963667219@qq.com>
2021-02-17 19:52:18 +0800

    初始化计数器项目的基本结构

--- vuex/src/App.vue
+++ vuex/src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="APP">
+    <A />
+    <h1>------------------</h1>
+    <B />
   </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import A from './components/Addition'
+import B from './components/Subtraction'
 export default {
-  name: 'App',
+  name: 'APP',
+  data() {
+    return {}
+  },
   components: {
-    HelloWorld
-  }
+    A,
+    B,
+  },
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ 
+<style scoped lang = "scss">
 </style>
---
+++ vuex/src/components/Addition.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="Addition">
+    <h3>当前最新的count值为:</h3>
+    <button @click="Addition">+1</button>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'Addition',
+  data() {
+    return {}
+  },
+  methods: {
+    Addition() {},
+  },
+}
+</script>
+ 
+<style scoped lang = "scss">
+</style>
