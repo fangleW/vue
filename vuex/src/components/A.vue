52aa439030250eed4cf24023fc2facb8ed4b6026
方乐 <1963667219@qq.com>
2021-02-17 19:33:18 +0800

    vuex分支测试

---
+++ vuex/src/components/A.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="A"></div>
+</template>
+ 
+<script>
+export default {
+  name: "A",
+  data() {
+    return {
+      msg: "Welcome to your vueName",
+    };
+  },
+};
+</script>
+ 
+<style scoped lang = "scss">
+</style>
