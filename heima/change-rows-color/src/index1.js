821b202be6b3ff9fa7c44c83ca30c33260263819
方乐 <1963667219@qq.com>
2021-09-09 21:42:18 +0800

    0908 提交

--- heima/change-rows-color/src/index1.js
+++ heima/change-rows-color/src/index1.js
@@ -1,6 +1,13 @@
 //ES6导入jquery
 import $ from 'jquery'
+//导入样式(在webpack中，一切皆模块)
+import './css/index.css'
+import './css/index.less'
 
+// 1.导入图片，得到图片文件
+import logo from './images/logo.jpg'
+// 2.给img标签设置src属性
+$('.box').attr('src',logo)
 $(function () {
   $('li:odd').css('background-color', 'red')
   $('li:even').css('background-color', 'green')
--- heima/change-rows-color/webpack.config.js
+++ heima/change-rows-color/webpack.config.js
@@ -26,7 +26,24 @@ module.exports = {
     //生成的文件名
     filename: 'main.js',
   },
-  plugins:[htmlPlugin]
+  plugins:[htmlPlugin],
+  //
+  devServer:{
+    open:true, //初次打包完成后，自动打开浏览器
+    //host:'127.0.0.1', //实时打包使用的地址
+    port: 80 //实时打包使用的端口号
+  },
+  module:{ //所有第三方文件模块的匹配规则
+    rules:[
+      //定义了不同模块对应的loader
+      {test:/\.css$/,use:['style-loader','css-loader']},
+      //处理less
+      {test:/\.less$/,use:['style-loader','css-loader','less-loader']},
+      //处理图片文件loader 
+      //use 需要调用的只有一个可以是字符串，多个必须是数组
+      {test:/\.jpg|png|gif$/,use:['url-loader']},
+    ]
+  }
 }
 
 
